bd159d48e0daef579762481c811a01b3690c4bfd
pradchan <pradchan@demo003.us.oracle.com>
2016-07-05 00:01:07 -0700

    Changed Maven Central Repo user

--- create-application.sh
+++ create-application.sh
@@ -2,15 +2,18 @@
 
 # ________REQUIRED INPUT PARAMETERS_________
 export ID_DOMAIN=$1
-export USER_ID=$2
-export USER_PASSWORD=$3
+#export USER_ID="gse-admin_ww@oracle.com"
+#export USER_PASSWORD="ZAQ!2wsx"
 export APP_NAME="TomcatPetClinic"
 
 export RUNTIME="java"
 export APAAS_HOST=apaas.us.oraclecloud.com
 
+export USER_ID=$(curl -s -X GET -H 'X-Oracle-Authorization: Basic Z3NlLWRldm9wc193d0BvcmFjbGUuY29tOjVjWmJzWkxuMQ==' 'https://adsweb.oracleads.com/apex/adsweb/parameters/democloud_admin_opc_email' | jq '.items[].value' | tr -d '"')
+export USER_PASSWORD=$(curl -s -X GET -H 'X-Oracle-Authorization: Basic Z3NlLWRldm9wc193d0BvcmFjbGUuY29tOjVjWmJzWkxuMQ==' 'https://adsweb.oracleads.com/apex/adsweb/parameters/democloud_admin_opc_password' | jq '.items[].value' | tr -d '"')
+
 if [ -z "${ID_DOMAIN}" ] || [ -z "$USER_ID" ] || [ -z "$USER_PASSWORD" ]; then
-  echo "usage: ${0} <id domain> <user id> <user password>";
+  echo "usage: ${0} <id domain> ";
   exit -1;
 fi
 
@@ -45,7 +48,7 @@ else
     -F "name=${APP_NAME}" \
     -F "runtime=${RUNTIME}" \
     -F "subscription=Hourly" \
-    -F "deployment=@deployment.json" \
+    -F "deployment=@/storage/cloud/bundles/appdev/refresh/jcode/apaas/petclinic/deployment.json" \
     https://${APAAS_HOST}/paas/service/apaas/api/v1.1/apps/${ID_DOMAIN}
 fi
 echo '\n[info] Complete\n'
